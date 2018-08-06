4c244d37db7c006fdc551841f1036ba5a28b1026
siri chandana <siriammulu2415@gmail.com>
2018-08-06 15:30:32 +0530

    first commit

---
+++ src/main/java/com/example/Spring_Seed/SpringSeedApplication.java
@@ -0,0 +1,12 @@
+package com.example.Spring_Seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSeedApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/Spring_Seed/SpringSeedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.Spring_Seed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSeedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
